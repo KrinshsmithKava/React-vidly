7ec0651c692737714788c1028d741234df5faeaa
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-11 11:38:35 +0530

    react

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,16 +19,42 @@ class LoginForm extends Component {
         console.log("Submitted");
     };
 
+    handleChange = ({ currentTarget: input }) => {
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({ account });
+    };
+
     render() {
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={this.handleSubmit}>
-                <div className="form-group"><label htmlFor="username">Username</label><input id="username" type="text" className="form-control" /></div>
-                <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control" /></div>
-                <button className="btn btn-primary">Login</button>
-            </form>
-        </div>)
+        const { account } = this.state;
+
+        return (
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            value={account.username}
+                            onChange={this.handleChange}
+                            id="username"
+                            name="username"
+                            type="text"
+                            className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            value={account.password}
+                            onChange={this.handleChange}
+                            name="password"
+                            id="password"
+                            type="text"
+                            className="form-control" />
+                    </div>
+                    <button className="btn btn-primary">Login</button>
+                </form>
+            </div>)
     }
 }
 
