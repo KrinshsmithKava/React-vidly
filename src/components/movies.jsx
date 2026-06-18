ad8a3f72a9a08997f3a9c751aa426b08e9688eda
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-18 12:51:26 +0530

    ract

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import MoviesTable from './moviesTable';
 import ListGroup from "./common/listGroup";
 import Pagination from './common/pagination';
@@ -6,7 +7,6 @@ import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from '../utils/paginate';
 import _ from 'lodash';
-import { Link } from "react-router-dom";
 import SearchBox from "./common/searchBox";
 
 
