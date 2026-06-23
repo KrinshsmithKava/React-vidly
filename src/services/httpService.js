5e333f3d224e533d8d0f5a1dc7d208986cc5da4f
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-23 17:18:09 +0530

    react

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,27 +1,30 @@
 import axios from "axios";
 import logger from './logService';
-import auth from './authService';
 import { toast } from 'react-toastify';
-
-axios.defaults.headers.common['x-auth-token'] = auth.getJwt();
 
 axios.interceptors.response.use(null, error => {
     const expectedError =
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status < 500;
-
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+    
     if (!expectedError) {
         logger.log(error);
         toast.error("An unexpected error occurrred.");
     }
-
+    
     return Promise.reject(error);
 });
+
+function setJwt(jwt) {
+    axios.defaults.headers.common['x-auth-token'] = jwt;
+}
+
 
 export default {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete
+    delete: axios.delete,
+    setJwt
 };
