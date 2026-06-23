81a0ed7a304757508ad8e9fd70334bfa0c143176
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-23 11:44:58 +0530

    react

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 // import Rentals from './rentals';
 // import NotFound from './notFound';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">Vidly</Link>
@@ -23,8 +23,25 @@ const NavBar = () => {
                     <NavLink className="nav-item nav-link" to="/movies">Movies</NavLink>
                     <NavLink className="nav-item nav-link" to="/customers">Customers</NavLink>
                     <NavLink className="nav-item nav-link" to="/rentals">Rentals</NavLink>
-                    <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
-                    <NavLink className="nav-item nav-link" to="/register">Register</NavLink>
+                    { !user && (
+                        <React.Fragment>
+                        <NavLink className="nav-item nav-link" to="/login">
+                            Login
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/register">
+                            Register
+                        </NavLink>
+                    </React.Fragment> )}
+
+                    { user && (
+                        <React.Fragment>
+                        <NavLink className="nav-item nav-link" to="/profile">
+                            {user.name}
+                        </NavLink>
+                        <NavLink className="nav-item nav-link" to="/logout">
+                            Logout
+                        </NavLink>
+                    </React.Fragment> )}
                 </div>
             </div>
         </nav>
