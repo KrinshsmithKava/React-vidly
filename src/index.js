32e48eed1cb98b40fa7cad8dbe1fad6b929fd31f
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-26 18:21:20 +0530

    react

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-console.log(process.env);
+console.log("SUPERMAN", process.env.REACT_APP_NAME);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
